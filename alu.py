cc92156c594d36ebb2acdf330af708e0cd64e3f6
Martín Gaitán <gaitan@gmail.com>
2010-10-25 16:51:22 -0300

    added zero detector signal to ALU output. A problem with the width of signed intbv make this still unconvertible

--- alu.py
+++ alu.py
@@ -13,6 +13,35 @@ from myhdl import Signal, delay, always_comb, always, Simulation, \
                   intbv, bin, instance, instances, now, toVHDL
 
 
+def alu_control(aluop, funct_field, control_out):
+    
+
+    @always_comb
+    def logic():
+        if aluop[0] and aluop[1]:
+            control_out.next = intbv('0010')
+
+        elif aluop[0]:
+            control_out.next = intbv('0110')
+
+        elif aluop[1]:
+            if bin(funct_field[3:]) == '0000':
+                control_out.next = intbv('0010')
+            
+            elif bin(funct_field[3:]) == '0010':
+                control_out.next = intbv('0110')
+                
+            elif bin(funct_field[3:]) == '0100':
+                control_out.next = intbv('0000')
+        
+            elif bin(funct_field[3:]) == '0101':
+                control_out.next = intbv('0001')
+        
+            elif bin(funct_field[3:]) == '1010':
+                control_out.next = intbv('0111')
+
+    return logic
+
 
 def alu(control, op1, op2, out_, zero):
     """
@@ -59,7 +88,7 @@ def alu(control, op1, op2, out_, zero):
             aux =  ~ (op1 | op2)   #TODO check this
            
 
-        if aux == intbv(0):
+        if aux == 0:
             zero.next = 1
         else:
             zero.next = 0
@@ -71,7 +100,7 @@ def alu(control, op1, op2, out_, zero):
 
 
 
-def testBench():
+def testBench_alu():
 
     control_i = Signal(intbv(0)[4:])
 
@@ -98,8 +127,32 @@ def testBench():
         
     return instances()
         
+def testBench_alu_control():
+
+    aluop_i = Signal(intbv(0)[2:])
+    funct_field_i = Signal(intbv(0)[6:])
+    alu_control_lines = Signal(intbv(0)[4:])
+
+    alu_control_i = alu_control(aluop_i, funct_field_i, alu_control_lines)
+
+    @instance
+    def stimulus():
+        for i in range(4):
+            aluop_i.next = intbv(i)
+
+            for j in range(2**6):
+
+                funct_field_i.next = intbv(j)
+
+                yield delay(10)
+                print "aluop: %s | funct field: %s | alu_control_lines: %s" % (bin(control_i, 2), bin(funct_field_i, 6 ), bin(alu_control_lines, 4)) 
+            
+    return instances()
+
+
+
 def main():
-    sim = Simulation(testBench())
+    sim = Simulation(testBench_alu_control())
     sim.run()
 
 if __name__ == '__main__':
