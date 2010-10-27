64f1ec86a2cf80e5e50a86f7628baa65c5c954b1
Martín Gaitán <gaitan@gmail.com>
2010-10-27 16:37:14 -0300

    splitted components designs

--- alu.py
+++ alu.py
@@ -13,42 +13,6 @@ from myhdl import Signal, delay, always_comb, always, Simulation, \
                   intbv, bin, instance, instances, now, toVHDL
 
 
-def alu_control(aluop, funct_field, control_out):
-    
-
-    @always_comb
-    def logic():
-        if not aluop[0] and not aluop[1]:
-            control_out.next = intbv('0010')
-
-        elif aluop[0]:
-            control_out.next = intbv('0110')
-
-        elif aluop[1]:
-           
-            if bin(funct_field[3:], 4) == '0000':
-                control_out.next = intbv('0010')
-            
-            elif bin(funct_field[3:], 4) == '0010':
-                control_out.next = intbv('0110')
-                
-            elif bin(funct_field[3:], 4) == '0100':
-                control_out.next = intbv('0000')
-        
-            elif bin(funct_field[3:], 4) == '0101':
-                control_out.next = intbv('0001')
-        
-            elif bin(funct_field[3:], 4) == '1010':
-                control_out.next = intbv('0111')
-
-            else:
-                control_out.next = intbv(0)
-        else:
-            control_out.next = intbv(0)
-
-
-    return logic
-
 
 def alu(control, op1, op2, out_, zero):
     """
@@ -89,7 +53,7 @@ def alu(control, op1, op2, out_, zero):
 
         elif control == int('0111',2):
             #TODO: set on less than
-            pass
+            out_.next = 0
 
         elif bin(control, 4) == '1100':
             out_.next =  ~ (op1 | op2)   #TODO check this
@@ -135,29 +99,6 @@ def testBench_alu():
             print "Control: %s | %i %s %i | %i | z=%i" % (bin(control_i, 4), op1_i, func, op2_i, out_i, zero_i) 
         
     return instances()
-        
-def testBench_alu_control():
-
-    aluop_i = Signal(intbv(0)[2:])
-    funct_field_i = Signal(intbv(0)[6:])
-    alu_control_lines = Signal(intbv(0)[4:])
-
-    alu_control_i = toVHDL(alu_control, aluop_i, funct_field_i, alu_control_lines)
-
-    @instance
-    def stimulus():
-        for i in range(4):
-            aluop_i.next = intbv(i)
-
-            for j in range(2**6):
-
-                funct_field_i.next = intbv(j)
-
-                yield delay(10)
-                print "aluop: %s | funct field: %s | alu_control_lines: %s" % (bin(aluop_i, 2), bin(funct_field_i, 6 ), bin(alu_control_lines, 4)) 
-            
-    return instances()
-
 
 
 def main():
---
+++ alu_control.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+
+"""
+ALU CONTROL
+"""
+
+import random
+
+from myhdl import Signal, delay, always_comb, always, Simulation, \
+                  intbv, bin, instance, instances, now, toVHDL
+
+
+
+def alu_control(aluop, funct_field, control_out):
+    
+
+    @always_comb
+    def logic():
+        if not aluop[0] and not aluop[1]:
+            control_out.next = intbv('0010')
+
+        elif aluop[0]:
+            control_out.next = intbv('0110')
+
+        elif aluop[1]:
+           
+            if bin(funct_field[3:], 4) == '0000':
+                control_out.next = intbv('0010')
+            
+            elif bin(funct_field[3:], 4) == '0010':
+                control_out.next = intbv('0110')
+                
+            elif bin(funct_field[3:], 4) == '0100':
+                control_out.next = intbv('0000')
+        
+            elif bin(funct_field[3:], 4) == '0101':
+                control_out.next = intbv('0001')
+        
+            elif bin(funct_field[3:], 4) == '1010':
+                control_out.next = intbv('0111')
+
+            else:
+                control_out.next = intbv(0)
+        else:
+            control_out.next = intbv(0)
+
+
+    return logic
+
+
+        
+def testBench_alu_control():
+
+    aluop_i = Signal(intbv(0)[2:])
+    funct_field_i = Signal(intbv(0)[6:])
+    alu_control_lines = Signal(intbv(0)[4:])
+
+    alu_control_i = toVHDL(alu_control, aluop_i, funct_field_i, alu_control_lines)
+
+    @instance
+    def stimulus():
+        for i in range(4):
+            aluop_i.next = intbv(i)
+
+            for j in range(2**6):
+
+                funct_field_i.next = intbv(j)
+
+                yield delay(10)
+                print "aluop: %s | funct field: %s | alu_control_lines: %s" % (bin(aluop_i, 2), bin(funct_field_i, 6 ), bin(alu_control_lines, 4)) 
+            
+    return instances()
+
+
+
+def main():
+    #sim = Simulation(testBench_alu_control())
+    sim = Simulation(testBench_alu_control())
+    sim.run()
+
+if __name__ == '__main__':
+    main()
