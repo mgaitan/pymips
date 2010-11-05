ab861be9407e0d9e396757b0b3229e9613ce61d0
Martín Gaitán <gaitan@gmail.com>
2010-11-05 14:38:20 -0300

    changing the way program counter works

--- program_counter.py
+++ program_counter.py
@@ -8,44 +8,59 @@ Program counter
 
 
 from myhdl import Signal, delay, always_comb, always, Simulation, \
-                  intbv, bin, instance, instances, now, toVHDL
+                  intbv, bin, instance, instances, now, toVHDL, traceSignals
 
 from clock_driver import clock_driver
 
+from alu import ALU
 
+def program_counter(clk, input, output):
+    """
+    A program counter that works as a latch. 
 
-def program_counter(clk, address):
+    clk : clock signal   . On posedge it refresh the output with it's internal state
+    input: the input count
+    output: address output
     """
-    clk : clock signal    
-    address: address output
-    """
+
+    state = Signal(intbv(0)[32:])
 
     @always(clk.posedge)
-    def inc():        
-        address.next = address + 1
+    def up_out():        
+        output.next = state.val
 
-    return inc
+    @always(clk.negedge)
+    def up_state():        
+        state.next = input.val      #this should be done in a separate instance
+
+    return up_out, up_state
 
 
 def testBench():
 
     clk = Signal(intbv(0)[1:])
-    S = Signal(intbv(0, min=0, max=32))
+    i = Signal(intbv(0, min=0, max=32))
+    o = Signal(intbv(0, min=0, max=32))
 
     clkdriver_inst = clock_driver(clk)
-    pc_inst = toVHDL(program_counter, clk, S)
+    pc_inst = program_counter( clk, i, o)
+
+    alu_i = ALU( Signal('0010'), o, Signal(1), i, Signal(0))
+
 
     @instance
     def stimulus():
         while True:
+            i.next = i + 1
             yield delay(1)
-            print "time: %s | Clock: %i | address: %i" % (now(), clk, S)
+            print "time: %s | Clock: %i | in: %i | out: %i" % (now(), clk, i, o)
 
     return instances()
 
 
 def main():
-    sim = Simulation(testBench())
+    tc = traceSignals(testBench)
+    sim = Simulation(tc)
     sim.run(20)
 
 if __name__ == '__main__':
