e018ffb1c939a5208bd9ee8fa4b397be4fb917b9
Martín Gaitán <gaitan@gmail.com>
2010-11-05 16:00:06 -0300

    fixed experimental program counter. it works as a clock triggered gate.

--- program_counter.py
+++ program_counter.py
@@ -16,27 +16,20 @@ from alu import ALU
 
 def program_counter(clk, input, output):
     """
-    A program counter that works as a latch. 
 
     clk : clock signal   . On posedge it refresh the output with it's internal state
     input: the input count
     output: address output
     """
 
-    state = Signal(intbv(0)[32:])
-
-    @always(clk.posedge)
-    def up_out():        
-        output.next = state.val
-
     @always(clk.negedge)
-    def up_state():        
-        state.next = input.val      #this should be done in a separate instance
+    def update():        
+        output.next = input
 
-    return up_out, up_state
+    return update
 
 
-def testBench():
+def pc_testbench():
 
     clk = Signal(intbv(0)[1:])
     i = Signal(intbv(0, min=0, max=32))
@@ -44,14 +37,16 @@ def testBench():
 
     clkdriver_inst = clock_driver(clk)
     pc_inst = program_counter( clk, i, o)
-
-    alu_i = ALU( Signal('0010'), o, Signal(1), i, Signal(0))
+    
+    
+    
+    c = Signal(0b0010)
+    alu_i = ALU( c, o, Signal(1), i, Signal(0))
 
 
     @instance
     def stimulus():
         while True:
-            i.next = i + 1
             yield delay(1)
             print "time: %s | Clock: %i | in: %i | out: %i" % (now(), clk, i, o)
 
@@ -59,7 +54,7 @@ def testBench():
 
 
 def main():
-    tc = traceSignals(testBench)
+    tc =  traceSignals(pc_testbench)
     sim = Simulation(tc)
     sim.run(20)
 
